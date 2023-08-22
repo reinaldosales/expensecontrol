8bdd06ac6ca29e872a075891debb356538988cf8
José Reinaldo Sales Soares Naldo <jose.naldo@fitbank.com.br>
2023-08-22 13:25:32 -0300

    feat: adicionando estrutura MVC do projeto

---
+++ ExpenseControl/ExpenseControl.MVC/Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace ExpenseControl.MVC.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
